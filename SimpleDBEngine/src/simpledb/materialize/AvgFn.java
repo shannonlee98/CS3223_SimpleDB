049c63e3ef003239c0ffd81a667a77ac1f9f4d7b
ZhengWen <e0310410@u.nus.edu>
2022-02-24 19:38:15 +0800

    Lab 5.2 (complete Avgfn, Minfn, Sumfn trivial implementation)

---
+++ SimpleDBEngine/src/simpledb/materialize/AvgFn.java
@@ -0,0 +1,62 @@
+package simpledb.materialize;
+
+import simpledb.query.Constant;
+import simpledb.query.Scan;
+
+/**
+ * The <i>avg</i> aggregation function.
+ */
+public class AvgFn implements AggregationFn {
+   private String fldname;
+   private int sum;
+   private int count;
+
+   /**
+    * Create a avg aggregation function for the specified field.
+    * @param fldname the name of the aggregated field
+    */
+   public AvgFn(String fldname) {
+      this.fldname = fldname;
+   }
+   
+   /**
+    * Start a new count.
+    * Since SimpleDB does not support null values,
+    * every record will be counted,
+    * regardless of the field.
+    * The current sum is thus set to first int val.
+    * The current count is set to 1.
+    * @see AggregationFn#processFirst(Scan)
+    */
+   public void processFirst(Scan s) {
+      sum = s.getInt(fldname);
+      count = 1;
+   }
+   
+   /**
+    * Since SimpleDB does not support null values,
+    * add the next int value to the sum
+    * add one to the count since we assume no null values
+    * @see AggregationFn#processNext(Scan)
+    */
+   public void processNext(Scan s) {
+      sum += s.getInt(fldname);
+      count ++;
+   }
+   
+   /**
+    * Return the field's name, prepended by "avgof".
+    * @see AggregationFn#fieldName()
+    */
+   public String fieldName() {
+      return "avgof" + fldname;
+   }
+   
+   /**
+    * Return the current avg.
+    * @see AggregationFn#value()
+    */
+   public Constant value() {
+      return new Constant(sum/count);
+   }
+}
